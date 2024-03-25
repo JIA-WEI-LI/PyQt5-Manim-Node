c3fa12f20d3a1009511e670b195ca9f32ecb9bc5
JIA-WEI-LI <magicsoldier19@gmail.com>
2024-03-25 20:27:42 +0800

    創建圖像視圖與場景

--- app/Node/NodeEditor_Window.py
+++ app/Node/NodeEditor_Window.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QGraphicsView, QGraphicsScene
 from PyQt5.QtGui import QIcon
 
 from config.icon import Icon
@@ -11,6 +11,18 @@ class NodeEditorWindow(QWidget):
 
     def initUI(self):
         self.setGeometry(200 ,200, 800, 600)
+
+        self.layout = QVBoxLayout()
+        self.layout.setContentsMargins(0, 0, 0, 0)
+        self.setLayout(self.layout)
+
+        # 創建圖像場景
+        self.graphicsScene = QGraphicsScene()
+
+        # 創建圖像視圖
+        self.view = QGraphicsView(self)
+        self.view.setScene(self.graphicsScene)
+        self.layout.addWidget(self.view)
 
         self.setWindowIcon(QIcon(Icon.WINDOW_LOGO))
         self.setWindowTitle("Manim Node Editor")

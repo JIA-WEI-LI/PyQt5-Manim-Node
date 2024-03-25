b78fa2942a0e7f46ab5ad603dffe8523248233f3
JIA-WEI-LI <magicsoldier19@gmail.com>
2024-03-25 16:50:00 +0800

    連結兩個檔案

---
+++ app/Node/NodeEditor_Window.py
@@ -0,0 +1,3 @@
+class NodeEditorWindow:
+    def __init__(self) -> None:
+        pass
--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 
+from .Node.NodeEditor_Window import NodeEditorWindow
+
 if __name__=="__main__":
     app = QApplication(sys.argv)
     window = NodeEditorWindow()

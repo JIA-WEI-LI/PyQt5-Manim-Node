12061ffaa0fca592448e97dea01aa31623ae7dcd
JIA-WEI-LI <magicsoldier19@gmail.com>
2024-03-25 20:46:12 +0800

    新增視窗大小判斷

--- app/Node/NodeEditor_Window.py
+++ app/Node/NodeEditor_Window.py
@@ -1,5 +1,9 @@
+import math
+
+from PyQt5.QtCore import QRectF
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QGraphicsView, QGraphicsScene
-from PyQt5.QtGui import QIcon, QColor
+from PyQt5.QtGui import QIcon, QPainter, QPen
+from PyQt5.QtCore import QLine
 
 from config.icon import Icon
 from config.palette import WindowColor
@@ -33,7 +37,23 @@ class NodeGraphicsScene(QGraphicsScene):
     def __init__(self, parent=None):
         super().__init__(parent)
 
+        self.gridSize = 20
         self.sceenWidth, self.sceenHeight = 640000, 640000
         self.setSceneRect(self.sceenWidth//2, self.sceenHeight//2, self.sceenWidth, self.sceenHeight)
 
+        self.penLight = QPen(WindowColor.DEFAULT_PEN_LIGHT)
+        self.penLight.setWidth(1)
         self.setBackgroundBrush(WindowColor.DEFAULT_BACKGROUND)
+
+    def drawBackground(self, painter: QPainter, rect: QRectF) -> None:
+        super().drawBackground(painter, rect)
+
+        left = int(math.floor(rect.left))
+        right = int(math.floor(rect.right))
+        top = int(math.floor(rect.top))
+        bottom= int(math.floor(rect.bottom))
+
+        lines_light = []
+
+        painter.setPen(self.penLight)
+        painter.drawLines(*lines_light)
--- app/config/palette.py
+++ app/config/palette.py
@@ -2,3 +2,5 @@ from PyQt5.QtGui import QColor
 
 class WindowColor:
     DEFAULT_BACKGROUND = QColor("#393939")
+    DEFAULT_PEN_LIGHT = QColor("2f2f2f")
+    DEFAULT_PEN_DARK = QColor("")

e4f9300adcddfa79ffadc70142e83e34c2864adc
JIA-WEI-LI <magicsoldier19@gmail.com>
2024-03-25 16:54:29 +0800

    製作傑典編輯器視窗

--- app/Node/NodeEditor_Window.py
+++ app/Node/NodeEditor_Window.py
@@ -1,3 +1,13 @@
-class NodeEditorWindow:
-    def __init__(self) -> None:
-        pass
+from PyQt5.QtWidgets import QWidget
+
+class NodeEditorWindow(QWidget):
+    def __init__(self, parent=None) -> None:
+        super().__init__(parent)
+
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(200 ,200, 800, 600)
+
+        self.setWindowTitle("Manim Node Editor")
+        self.show()
--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 
-from .Node.NodeEditor_Window import NodeEditorWindow
+from Node.NodeEditor_Window import NodeEditorWindow
 
 if __name__=="__main__":
     app = QApplication(sys.argv)
     window = NodeEditorWindow()
 
-    sys.exit(app.exec_)
+    sys.exit(app.exec_())

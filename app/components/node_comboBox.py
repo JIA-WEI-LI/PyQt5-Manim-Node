a0bee88f4beae27cf5b312441c519c8f64f860a6
JIA-WEI-LI <magicsoldier19@gmail.com>
2024-04-08 00:42:07 +0800

    修正 ComboBox 內部 item 沒有圓角問題

--- app/components/node_comboBox.py
+++ app/components/node_comboBox.py
@@ -8,13 +8,20 @@ class CustomItemDelegate(QStyledItemDelegate):
         option_copy = QStyleOptionViewItem(option)
         # 設定每個元素的高度
         option_copy.rect.setHeight(25)
+
+        border_radius = 5
+        fill_rect = option.rect.adjusted(1, 1, -1, -1)
         
         if option.state & QStyle.StateFlag.State_MouseOver:
-            painter.fillRect(option.rect, QColor('#464646'))
+            painter.setBrush(QColor('#464646'))
+            painter.setPen(Qt.PenStyle.NoPen)
+            painter.drawRoundedRect(fill_rect, border_radius, border_radius)
         elif option.state & QStyle.StateFlag.State_Selected:
-            painter.fillRect(option.rect, QColor('#4772b3'))
+            painter.setBrush(QColor('#4772b3'))
+            painter.setPen(Qt.PenStyle.NoPen)
+            painter.drawRoundedRect(fill_rect, border_radius, border_radius)
         else:
-            painter.fillRect(option.rect, QColor('#181818'))
+            painter.fillRect(option.rect, QColor(0, 0, 0, 0))
 
         # 繪製元素內容
         self.initStyleOption(option_copy, index)
@@ -35,14 +42,31 @@ class ComboBox(QComboBox):
         self.text_label.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
         self.text_label.setStyleSheet('color: #FFFFFF;')
 
+        # 滑鼠進入和離開事件處理程序
+        self.enterEvent = self.mouse_enter
+        self.leaveEvent = self.mouse_leave
+        self.is_mouse_over = False
+
+    def mouse_enter(self, event):
+        self.is_mouse_over = True
+        self.repaint()
+
+    def mouse_leave(self, event):
+        self.is_mouse_over = False
+        self.repaint()
+
     def paintEvent(self, event):
         painter = QPainter(self)
         rect = self.rect()
         border_radius = 3
         padding = 1
 
+        fill_color = QColor('#242424')
+        if self.is_mouse_over:
+            fill_color = QColor('#303030')
+
         fill_rect = rect.adjusted(padding, padding, -padding, -padding)
-        painter.fillRect(fill_rect, QColor('#242424'))
+        painter.fillRect(fill_rect, fill_color)
 
         painter.setPen(QPen(QColor('#464646'), 0.5))
 
@@ -57,13 +81,14 @@ class ComboBox(QComboBox):
         if current_index >= 0:
             current_text = self.itemText(current_index)
             self.text_label.setText(current_text)
-            # 在設置完文本後，重新計算 text_label 的大小
             self.text_label.adjustSize()
-            # 計算 text_label 的位置，使其置中於 ComboBox 的高度
             text_label_height = self.text_label.height()
             combobox_height = self.height()
             text_label_y = int((combobox_height - text_label_height) / 2)
             self.text_label.move(5, text_label_y)
 
-# 使用方法
-# custom_combo_box = CustomComboBox(parent)
+    def showPopup(self):
+        '''Override the default showPopup method to apply custom styling when the popup is shown.'''
+        super().showPopup()
+        # Set the popup's bottom border to have rounded corners
+        self.view().setStyleSheet('border-bottom-left-radius: 10px; border-bottom-right-radius: 10px;')

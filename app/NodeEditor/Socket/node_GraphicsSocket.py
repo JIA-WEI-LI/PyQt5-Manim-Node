6aadac00da7bcdffb41809b5b1e2ca5a4346f608
JIA-WEI-LI <magicsoldier19@gmail.com>
2024-04-08 13:52:40 +0800

    新增顏色代碼整理

--- app/NodeEditor/Socket/node_GraphicsSocket.py
+++ app/NodeEditor/Socket/node_GraphicsSocket.py
@@ -5,8 +5,9 @@ from PyQt5.QtGui import QPen, QBrush, QPainter, QColor
 from common.color_sheet import color_manager
 from config.debug import DebugMode
 
-PEN_COLOR = color_manager.get_color("SocketColor", "DEFAULT_OUTLINE")
-BRUSH_COLORLIST = color_manager.get_color_list("SocketColor", "DEFAULT_COLOR_LIST")
+class SocketColor:
+    PEN_COLOR = color_manager.get_color("SocketColor", "DEFAULT_OUTLINE")
+    BRUSH_COLORLIST = color_manager.get_color_list("SocketColor", "DEFAULT_COLOR_LIST")
 
 class NodeGraphicsSocket(QGraphicsItem):
     def __init__(self, socket, socket_type=1 ,parent=None) -> None:
@@ -16,10 +17,10 @@ class NodeGraphicsSocket(QGraphicsItem):
         self._radius = 7.0
         self._outline_width = 1.0
         # self._pen = QPen(SocketColor.DEFAULT_OUTLINE)
-        self._pen = QPen(PEN_COLOR)
+        self._pen = QPen(SocketColor.PEN_COLOR)
         self._pen.setWidthF(self._outline_width)
         # self.brush = QBrush(SocketColor.DEFAULT_COLOR_LIST[socket_type])
-        self.brush = QBrush(QColor(BRUSH_COLORLIST[socket_type]))
+        self.brush = QBrush(QColor(SocketColor.BRUSH_COLORLIST[socket_type]))
 
     def paint(self, painter:QPainter, QStyleOptionGraphicsItem, widget=None):
         painter.setBrush(self.brush)

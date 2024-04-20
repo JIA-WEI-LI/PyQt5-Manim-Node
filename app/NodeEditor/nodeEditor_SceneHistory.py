a9270ebeff07c4d6f0498594a1a4c1858f02d572
JIA-WEI-LI <magicsoldier19@gmail.com>
2024-04-20 16:28:01 +0800

    完成手動儲存步驟

--- app/NodeEditor/nodeEditor_SceneHistory.py
+++ app/NodeEditor/nodeEditor_SceneHistory.py
@@ -1,3 +1,4 @@
+from .Edge.node_GraphicsEdge import NodeGraphicsEdge
 from config.debug import DebugMode
 
 DEBUG = DebugMode.NODEEEDITOR_SCENEHISTORY
@@ -14,19 +15,20 @@ class SceneHistory():
         if DEBUG: print("UNDO")
 
         if self.history_current_step > 0:
-            self.history_current_step -=1
+            self.history_current_step -= 1
             self.restoreHistory()
 
     def redo(self):
-        if DEBUG: print("REDO")
+        if DEBUG: print("REDO: ", self.history_current_step)
 
         if self.history_current_step + 1 < len(self.history_stack):
-            self.history_current_step -= 1
+            self.history_current_step += 1
             self.restoreHistory()
 
     def restoreHistory(self):
         if DEBUG: print("Restoring history ... current_step: @%d" % self.history_current_step, 
                         "(%d)" % len(self.history_stack))
+        self.restoreHistoryStamp(self.history_stack[self.history_current_step])
             
     def storeHistory(self, desc):
         if DEBUG: print("Storing history", '"%s"' % desc,
@@ -48,7 +50,38 @@ class SceneHistory():
         if DEBUG: print(" -- setting step to: ", self.history_current_step)
 
     def createHistoryStamp(self, desc):
-        return desc
+        self_obj = {
+            'nodes': [],
+            'edges': [],
+        }
+
+        for item in self.scene.nodeGraphicsScene.selectedItems():
+            if hasattr(item, 'node'):
+                self_obj['nodes'].append(item.node.id)
+            elif isinstance(item, NodeGraphicsEdge):
+                self_obj['edges'].append(item.edge.id)
+
+        history_stamp = {
+            'desc': desc,
+            'snapshot': self.scene.serialize(),
+            'selection': self_obj
+        }
+        return history_stamp
             
     def restoreHistoryStamp(self, history_stamp):
-        if DEBUG: print("RHS: ", history_stamp)
+        if DEBUG: print("RHS: ", history_stamp['desc'])
+
+        self.scene.deserialize(history_stamp['snapshot'])
+
+        # 回復選擇
+        for edge_id in history_stamp['selection']['edges']:
+            for edge in self.scene.edges:
+                if edge.id == edge.id:
+                    edge.nodeGraphicsEdge.setSelected(True)
+                    break
+
+        for node_id in history_stamp['selection']['nodes']:
+            for node in self.scene.nodes:
+                if node.id == node.id:
+                    node.graphicsNode.setSelected(True)
+                    break

0ccc1864c155ccf42daab11702769741e49a3f15
JIA-WEI-LI <magicsoldier19@gmail.com>
2024-05-01 21:24:59 +0800

    Revert "修正測試節點內容"
    This reverts commit d4c6b354220aa9d3b9aea22d160d5c7237b7f173.

--- app/NodeEditorNodes/Testing_node.py
+++ app/NodeEditorNodes/Testing_node.py
@@ -2,12 +2,13 @@ from BlenderStyleWidget import *
 from NodeEditor.Node.node_Node import Node
 
 class Node_Test(Node):
-    def __init__(self, scene, title="Node_Test", input=[1, 1, 1], output=[2, 2, 2]):
+    def __init__(self, scene, title="Node_Test", input=[1, 2, 3], output=[1, 2, 3, 1]):
         super().__init__(scene, title, input, output)
 
-        self.content.addLabel("Output 1", isOutput=True)
-        self.content.addLabel("Output 2", isOutput=True)
-        self.content.addLabel("Output 3", isOutput=True)
-        self.content.addLabel("Input 1")
-        self.content.addLabel("Input 2")
-        self.content.addLabel("Input 3")
+        self.content.addLabel("Output", isOutput=True)
+        self.content.addLineEdit("Line Edit", tooltip="New Tooltip")
+        self.content.addProgressBar(tooltip="測試文件")
+        self.content.addComboBox(["List 1", "List 1", "List 1"])
+        self.content.addLabel("Test Label 1")
+        self.content.addLabel("Test Label 2")
+        self.content.addLabel("Test Label 3")

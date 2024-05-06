8fe8008ddd22f94f70850eb2d852e0b17425c78c
JIA-WEI-LI <magicsoldier19@gmail.com>
2024-05-06 21:26:49 +0800

    修正反序列化節點高度錯誤問題

--- nodeeditor/NodeEditorWindow/Node/node_Node.py
+++ nodeeditor/NodeEditorWindow/Node/node_Node.py
@@ -135,7 +135,7 @@ y = titleHeight: {int(self.graphicsNode.titleHeight)} \
         
         data['inputs'].sort(key=lambda socket: socket['index'] + socket['position'] * 10000)
         data['outputs'].sort(key=lambda socket: socket['index'] + socket['position'] * 10000)
-        self.graphicsNode.height = self.graphicsNode.titleHeight + 2 * self.graphicsNode.padding + len(data['inputs'] + data['outputs']) * self.socketSpace
+        self.graphicsNode.height = self.graphicsNode.titleHeight + self.graphicsNode.padding
 
         self.inputs, self.outputs = [], []
         for socket_data in data['inputs']:
@@ -149,6 +149,6 @@ y = titleHeight: {int(self.graphicsNode.titleHeight)} \
 
         self.content.deserialize(data['content'], hashmap)
         # TODO：暫時使用額外儲存節點內部高度解決變形問題
-        self.content.setFixedHeight(data['content_height'])
+        # self.content.setFixedHeight(data['content_height'])
 
         return True

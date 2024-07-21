97ca9b4f3002e613fad9184534c369ea59a433b2
JIA-WEI-LI <magicsoldier19@gmail.com>
2024-07-22 01:46:26 +0800

    刪除多餘程式碼

--- nodeeditor/NodeEditorWindow/BlenderStyleWidget/content_BaseSetting.py
+++ nodeeditor/NodeEditorWindow/BlenderStyleWidget/content_BaseSetting.py
@@ -1,6 +1,3 @@
-from PyQt5.QtWidgets import QSizePolicy
-
-# from common.color_sheet import color_manager
 from common.style_sheet import StyleSheet
 
 class ContentBaseSetting:
@@ -8,20 +5,7 @@ class ContentBaseSetting:
         super().__init__(*args, **kwargs)
         self.tooltip = kwargs.get("tooltip", "")
         self.debug = kwargs.get("debug", False)
-
-        # self.color_BACKGROUND_WINDOW = color_manager.get_color("BLENDERCOLOR", "BACKGROUND_WINDOW")
-        # self.color_GRAY_24 = color_manager.get_color("BLENDERCOLOR", "GRAY_24")
-        # self.color_GRAY_2a = color_manager.get_color("BLENDERCOLOR", "GRAY_2a")
-        # self.color_GRAY_30 = color_manager.get_color("BLENDERCOLOR", "GRAY_30")
-        # self.color_GRAY_46 = color_manager.get_color("BLENDERCOLOR", "GRAY_46")
-        # self.color_GRAY_54 = color_manager.get_color("BLENDERCOLOR", "GRAY_54")
-        # self.color_GRAY_65 = color_manager.get_color("BLENDERCOLOR", "GRAY_65")
-        # self.color_clicked = color_manager.get_color("BLENDERCOLOR", "CLICKED")
-        # self.color_mouseover = color_manager.get_color("BLENDERCOLOR", "HOVER")
-
         self.content_height = kwargs.get("content_height", 24)
-
-        # if apply_style: StyleSheet.applyStyle("node_content", self)
 
     def styles_set(self):
         self.setToolTip(self.tooltip)
